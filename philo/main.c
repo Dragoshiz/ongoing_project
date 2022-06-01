9db2bb1391ca6180a611c49b6dc5e30df6892af1
Dragos Imbrea <dragosimbrea@gmail.com>
2022-06-01 15:31:21 +0200

    starting philosophers project

--- philo/main.c
+++ philo/main.c
@@ -1,22 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dimbrea <dimbrea@student.42wolfsburg.de>   +#+  +:+       +#+        */
+/*   By: dimbrea <dimbrea@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/03 12:35:42 by dimbrea           #+#    #+#             */
-/*   Updated: 2022/01/04 14:40:51 by dimbrea          ###   ########.fr       */
+/*   Created: 2022/06/01 15:25:12 by dimbrea           #+#    #+#             */
+/*   Updated: 2022/06/01 15:25:13 by dimbrea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-Description Outputs the character ’c’ to the given file
-descriptor.
-*/
-#include "libft.h"
-
-void	ft_putchar_fd(char c, int fd)
-{
-	write(fd, &c, 1);
-}
--- philo/philo.h
+++ philo/philo.h
@@ -1,24 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shoppe <shoppe@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*   By: dimbrea <dimbrea@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/15 15:51:00 by dimbrea           #+#    #+#             */
-/*   Updated: 2022/01/15 21:24:30 by shoppe           ###   ########.fr       */
+/*   Created: 2022/06/01 15:25:09 by dimbrea           #+#    #+#             */
+/*   Updated: 2022/06/01 15:29:45 by dimbrea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* Returns the last node of the list. */
+#ifndef PHILO_H
+# define PHILO_H
+# include <sys/time.h>
+# include <stdio.h>
+	
 
-#include "libft.h"
-
-t_list	*ft_lstlast(t_list *lst)
-{
-	if (!lst)
-		return (NULL);
-	while (lst->next != NULL)
-		lst = lst->next;
-	return (lst);
-}
+# endif
